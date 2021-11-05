4c32f11bbdcda0143fdb2b1d5714a269fc44e856
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-11-05 08:09:38 -0400

    feat: adding workshop assignment for react

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -6,24 +6,30 @@ class Student {
         this.email = email;
         this.community = community;
     }
-    registerStudent(student) {
-        this.students.push(student);
-        //const studentAlreadyAdded = student.filter(student => student.email === this.email);
-    }
 }
 
 
 class Bootcamp {
-    constructor(name, level, students = [], preStudents = []) {
+    constructor(name, level, students = [], emails = []) {
         this.name = name;
         this.level = level;
         this.students = students;
-        this.preStudents = preStudents;
+        this.emails = emails; // add email property to Bootcamp
     }
-    // work on this later
-    addStudent(student) {
-        this.students.push(preStudents);
-        let preStudents = preStudent.filter(student => student.email === this.email);
+    registerStudent(student) {
+        //if (this.students.indexOf(student) === -1) {
+        //this.emails.push(student.email);
+        //console.log("!!!!", this.emails);
+        if (this.emails.indexOf(student.email) === -1) {
+            this.students.push(student.name);
+            console.log(`Registering ${student.name} to the bootcamp Web Dev Fundamentals`);
+            this.emails.push(student.email);
+        } else {
+            console.log(`${student.name} is already registered.`);
+
+        }
+
+        console.log(this.students);
     }
 }
 
@@ -32,6 +38,18 @@ const student1 = new Student('John', 'test@email.com', 'javascript');
 
 console.log(student1);
 
-const student2 = new Student('John', 'test@email.com', 'javascript');
+const student2 = new Student('Tim', 'test@email.com', 'javascript');
 
 console.log(student2);
+
+const student3 = new Student('Bob', 'check@email.com', 'javascript');
+
+console.log(student3);
+
+const bootcamp = new Bootcamp('Code 301', 'intermediate');
+
+bootcamp.registerStudent(student1);
+
+bootcamp.registerStudent(student2);
+
+bootcamp.registerStudent(student3);
