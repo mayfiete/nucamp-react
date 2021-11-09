6cf622d52e481ac6f5d9972270f51846785d0f2e
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-11-09 05:14:17 -0500

    first single page app render

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,20 +1,23 @@
 
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">NuCamp</NavbarBrand>
+          </div>
+        </Navbar>
+        <Directory />
+      </div>
+    );
+  }
 }
 
+// this file is a module because there is an export 
 export default App;
