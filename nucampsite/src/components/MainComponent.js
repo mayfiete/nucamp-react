c7177e6b5aa526672a5be942e3d4dfa97b1757d7
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-11-16 06:47:52 -0500

    feat: Footer checkpoint

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { CAMPSITES } from '../shared/campsites'
 
 
@@ -26,6 +27,7 @@ class Main extends Component {
                 <Header />
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Footer />
             </div>
         );
     }
