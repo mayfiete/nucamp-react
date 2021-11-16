3f1ae6210847dacfa0b50f5398385e79edfbf2cc
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-11-16 06:33:22 -0500

    feat: checkpoint in Header and Footer exercise

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -1,8 +1,8 @@
 
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Header from './HeaderComponent';
 import { CAMPSITES } from '../shared/campsites'
 
 
@@ -23,11 +23,7 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">NuCamp</NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header />
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
             </div>
