77dfb9e7e2053813148623abfe3cb12a13c494e0
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-11-26 16:16:34 -0500

    feat: recovery point

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -4,15 +4,110 @@ import React, { Component } from 'react';
 
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Button, Label, Col, Row, FormGroup } from 'reactstrap';
+
+
+/* 
+    construct a Reactstrap Modal that contains a React Redux form for users 
+    to submit their comments as shown in the image, as detailed below. 
+
+    Implement the rating field with Control.select and options 1-5, 
+    the author field with Control.text, and the comment text field with Control.textarea and 6 rows. 
+    For the model of the author, use ".author". For the model of the rating, use ".rating". 
+    For the model of the comment text, use ".text". The id and name for each should be based on the model. 
+    Refer to ContactComponent.js for examples of using LocalForm and the Control component.
+
+    Make sure that for every Control component, the name and id properties are the same as the model, 
+    without the dot (e.g. model=".text" id="text" name="text"). This is important in how the values are 
+    passed to the handleSubmit() method and used in the corresponding action creator.
+
+    For formatting: You do not need to use the Bootstrap grid for this (so no Rows or Cols). 
+    Format this in a way similar to the Login modal form, but instead of using the 
+    FormControl component (as that is not available for use with LocalForm), 
+    use a div with className="form-group" around each set of Controls and their corresponding Labels. 
+    You should end up with three divs inside the LocalForm. For all Controls, 
+    set className="form-control".
+
+*/
 
 class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rating: 0,
+            author: '',
+            text: '',
+            touched: {
+                ratings: false,
+                author: false,
+                text: false
+            }
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
+        /* 
+        this.handleRatingChange = this.handleRatingChange.bind(this);
+        this.handleAuthorChange = this.handleAuthorChange.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        */
+    }
+
+    handleSubmit(values) {
+        console.log("Current State is: " + JSON.stringify(values));
+        alert("Current State is: " + JSON.stringify(values));
+        // event.preventDefault();
+    }
+
 
     render() {
         return (
-            <button outline className="fa-lg">
-                <span className="fa fa-pencil-square-o">Submit Comment</span>
-            </button>
+            <React.Fragment>
+                <button outline className="fa-lg">
+                    <span className="fa fa-pencil-square-o">Submit Comment</span>
+                </button>
+                <div>
+                    <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                        <Row className="form-group">
+                            <Label htmlFor="rating" md={2}>Rating</Label>
+                            <Col md={10}>
+                                <LocalForm onSubmit={(values) => this.props.onSubmit(values.rating, values.author, values.text)}>
+                                    <div className="form-group">
+                                        <Control.select
+                                            model=".rating"
+                                            placeholder="Rating"
+                                            id="rating"
+                                            name="rating"
+                                            className="form-control"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <Control.text
+                                            model=".author"
+                                            id="author"
+                                            name="author"
+                                            placeholder="Your Name"
+                                            className="form-control"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <Control.textarea
+                                            model=".text"
+                                            id="text"
+                                            name="text"
+                                            placeholder="Comment"
+                                            className="form-control"
+                                        />
+                                    </div>
+                                </LocalForm>
+                            </Col>
+                        </Row>
+                    </LocalForm>
+                </div>
+            </React.Fragment>
         );
+
     }
 }
 
