6b77b565db5b53b61c08b4da51fee995d139514d
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-12-03 21:20:31 -0500

    feat: React Animation Components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -8,6 +8,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Button, Label, Modal, ModalHeader, ModalBody, } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const required = val => val && val.length;
@@ -148,12 +149,18 @@ class CommentForm extends Component {
 function RenderCampsite({ campsite }) {
     return (
         <div className="col-md-5 m-1" >
-            <Card>
-                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText> {campsite.description} </CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         </div>
     )
 }
@@ -164,16 +171,22 @@ function RenderComments({ comments, postComment, campsiteId }) {
             <React.Fragment>
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    {
-                        comments.map(comment => {
-                            return (
-                                <div key={comment.id}>
-                                    <p>{comment.text}</p>
-                                    <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                                </div>
-                            );
-                        })
-                    }
+                    <Stagger in>
+                        {
+                            comments.map(comment => {
+                                return (
+                                    <Fade in key={comment.id}>
+                                        <div>
+                                            <p>
+                                                {comment.text}<br />
+                                                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                                            </p>
+                                        </div>
+                                    </Fade>
+                                );
+                            })
+                        }
+                    </Stagger>
                     <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
 
--- nucampsite/src/components/HomeComponent.js
+++ nucampsite/src/components/HomeComponent.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
-import { baseUrl } from '../shared/baseUrl';
+import { baseUrl } from '../shared/baseUrl'; 
+import  { FadeTransform } from 'react-animation-components';
 
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
@@ -13,6 +14,11 @@ function RenderCard({ item, isLoading, errMess }) {
     }
     {
         return (
+            <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(50%)'
+            }}>
             <Card>
                 <CardImg src={baseUrl + item.image} alt={item.name} />
                 <CardBody>
@@ -20,6 +26,7 @@ function RenderCard({ item, isLoading, errMess }) {
                     <CardText>{item.description}</CardText>
                 </CardBody>
             </Card>
+        </FadeTransform>
         );
     }
 }
